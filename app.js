7dd32beced056682ec96ac9754c112ccd52f0a17
Nicolas Beaulieu <nicolas.beaulieu2000@gmail.com>
2023-02-01 10:41:41 -0500

    Add and UI interface for the Rock Paper scissor game

--- app.js
+++ app.js
@@ -1,59 +1,136 @@
 
 const POSSIBLE_CHOICE = ["roche", "papier", "ciseaux"]
-let score = 0
+let playerScore = 0
+let computerScore = 0
+let rockButton = document.getElementById('roche')
+let paperButton = document.getElementById('papier')
+let scissorButton = document.getElementById('ciseaux')
+let resultDiv = document.querySelector('.result')
+let gameResultP = document.createElement('p')
+let scoreDiv = document.querySelector('.score')
+let scoreP = document.createElement('p')
+let playerPick = ""
 
-function getPlayerChoice(){
-    return prompt("Qu'elle est votre choix: Roche, Papier ou ciseaux?").toLowerCase()
-}
+rockButton.addEventListener('click', function(){
+    playerPick = 'roche'
+    let computerChoice = getComputerChoice()
+    checkWinner(playerPick, POSSIBLE_CHOICE[computerChoice])
+    giveScore()
+    console.log(playerScore)
+    if(playerScore === 5){
+        printWinner()
+        setTimeout(() =>{
+            document.location.reload()
+        }, 3000)
+    }else if(computerScore === 5){
+        printLoser()
+        setTimeout(() =>{
+            document.location.reload()
+        }, 3000)
+    }
+})
+paperButton.addEventListener('click', function(){
+    playerPick = 'papier'
+    let computerChoice = getComputerChoice()
+    checkWinner(playerPick, POSSIBLE_CHOICE[computerChoice])
+    giveScore()
+    if(playerScore === 5){
+        printWinner()
+        setTimeout(() =>{
+            document.location.reload()
+        }, 3000)
+    }else if(computerScore === 5){
+        printLoser()
+        setTimeout(() =>{
+            document.location.reload()
+        }, 3000)
+    }
+})
+scissorButton.addEventListener('click', function(){
+    playerPick = 'ciseaux'
+    let computerChoice = getComputerChoice()
+    checkWinner(playerPick, POSSIBLE_CHOICE[computerChoice])
+    giveScore()
+    if(playerScore === 5){
+        printWinner()
+        setTimeout(() =>{
+            document.location.reload()
+        }, 3000)
+    }else if(computerScore === 5){
+        printLoser()
+        setTimeout(() =>{
+            document.location.reload()
+        }, 3000)
+    }
+})
+
 
 function getComputerChoice(){
     return Math.floor(Math.random() * POSSIBLE_CHOICE.length)
 }
 
 function checkWinner(playerSelection, computerSelection){
-    if(playerSelection === "roche" && computerSelection === "roche"){
-        alert("Égalité! Vous avez tout les deux choisies roche!")
+    if(playerSelection === computerSelection){
+        gameResultP.textContent = "Égalité!"
+        resultDiv.appendChild(gameResultP)
     }else if(playerSelection === "roche" && computerSelection === "papier"){
-        alert("Vous avez perdu! L'ordinateur a choisie papier!")
+        displayLosingResult(computerSelection)
+        computerScore++
     }else if(playerSelection === "roche" && computerSelection === "ciseaux"){
-        alert("Vous avez gagné! L'ordinateur a choisie ciseaux!")
-        score++
-    }else if(playerSelection === "papier" && computerSelection === "papier"){
-        alert("Égalité! Vous avez tout les deux choisies papier!")
+        displayWinningResult(computerSelection)
+        playerScore++
     }else if(playerSelection === "papier" && computerSelection === "roche"){
-        alert("Vous avez gagné! L'ordinateur a choisie roche!")
-        score++
+        displayWinningResult(computerSelection)
+        playerScore++
     }else if(playerSelection === "papier" && computerSelection === "ciseaux"){
-        alert("Vous avez perdu! L'ordinateur a choisie ciseaux!")
-    }else if(playerSelection === "ciseaux" && computerSelection === "ciseaux"){
-        alert("Égalité! Vous avez tout les deux choisies ciseaux!")
+        displayLosingResult(computerSelection)
+        computerScore++
     }else if(playerSelection === "ciseaux" && computerSelection === "roche"){
-        alert("Vous avez perdu! L'ordinateur a choisie roche!")
+        displayLosingResult(computerSelection)
+        computerScore++
     }else if(playerSelection === "ciseaux" && computerSelection === "papier"){
-        alert("Vous avez gagné! L'ordinateur a choisie papier!")
-        score++
+        displayWinningResult(computerSelection)
+        playerScore++
     }
     
 }
 
 function playGame(){
-    let userChoice = getPlayerChoice()
+    let userChoice = playerChoice
     let computerChoice = getComputerChoice()
     checkWinner(userChoice, POSSIBLE_CHOICE[computerChoice])
 }
 
-function giveScore(score){
-    if(score === 5 || score == 4){
-        alert(`Votre score est de ${score}, très bien jouer!`)
-    }else if(score === 3){
-        alert(`Votre score est de ${score}, vous avez gagné de justesse!`)
-    }else{
-        alert(`Votre score est de ${score}, Vous avez perdu!`)
-    }
+function giveScore(){
+    scoreP.innerText = `Score Joueur: ${playerScore}` + ` Score Ordinateur: ${computerScore}`
+    scoreDiv.appendChild(scoreP)
     
 }
 
-for(let i = 0; i < 5; i++){
-    playGame()
+function displayLosingResult(computerPick){
+    gameResultP.textContent = `Vous avez perdu! L'ordinateur a choisi: ${computerPick}`
+    resultDiv.appendChild(gameResultP)
 }
-giveScore(score)
+
+function displayWinningResult(computerPick){
+    gameResultP.textContent = `Vous avez gagné! L'ordinateur a choisi: ${computerPick}`
+    resultDiv.appendChild(gameResultP)
+}
+
+function printWinner(){
+    scoreP.innerText = 'Vous avez gagné la partie! Bravo!'
+}
+
+function printLoser(){
+    scoreP.innerText = "Vous avez perdu la partie!"
+}
+
+
+gameResultP.style.color = 'gold'
+gameResultP.style.fontSize = '30px'
+scoreP.style.color = 'gold'
+scoreP.style.fontSize = '30px'
+
+
+
+
